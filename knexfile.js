ab1a1de6ab11c1ebecd5dff717f34c53c718926d
Jon Willemain <jon.willemain@gmail.com>
2017-05-30 18:40:48 -0600

    Add knex file

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/byob',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/byob_test',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/test',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+  },
+};
