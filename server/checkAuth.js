8f0154fac7052cc9990d3d48ef35fac0de1eeb63
Jon Willemain <jon.willemain@gmail.com>
2017-06-06 16:36:03 -0600

    remove check auth file

--- server/checkAuth.js
+++ server/checkAuth.js
@@ -1,38 +1,38 @@
-const express = require('express');
-
-const app = express();
-const jwt = require('jsonwebtoken');
-const config = require('dotenv').config().parsed;
-
-if (process.env.NODE_ENV === 'development' && (!config.CLIENT_SECRET || !config.USERNAME || !config.PASSWORD)) {
-  throw new Error('Make sure you have a CLIENT_SECRET, USERNAME, and PASSWORD in your .env file');
-}
-
-app.set('secretKey', process.env.CLIENT_SECRET || config.CLIENT_SECRET);
-const token = jwt.sign('token', app.get('secretKey'));
-
-const checkAuth = (request, response, next) => {
-  const authToken = request.body.token ||
-                request.params.token ||
-                request.headers.authorization;
-  if (authToken) {
-    jwt.verify(authToken, app.get('secretKey'), (error, decoded) => {
-      if (error) {
-        return response.status(403).send({
-          success: false,
-          messgae: 'Invalid authorization token',
-        });
-      } else {
-        request.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    return response.status(403).send({
-      success: false,
-      messgae: 'You must be authorized to hit this end point',
-    });
-  }
-};
-
-module.exports = checkAuth;
+// const express = require('express');
+//
+// const app = express();
+// const jwt = require('jsonwebtoken');
+// const config = require('dotenv').config().parsed;
+//
+// if (process.env.NODE_ENV === 'development' && (!config.CLIENT_SECRET || !config.USERNAME || !config.PASSWORD)) {
+//   throw new Error('Make sure you have a CLIENT_SECRET, USERNAME, and PASSWORD in your .env file');
+// }
+//
+// app.set('secretKey', process.env.CLIENT_SECRET || config.CLIENT_SECRET);
+// const token = jwt.sign('token', app.get('secretKey'));
+//
+// const checkAuth = (request, response, next) => {
+//   const authToken = request.body.token ||
+//                 request.params.token ||
+//                 request.headers.authorization;
+//   if (authToken) {
+//     jwt.verify(authToken, app.get('secretKey'), (error, decoded) => {
+//       if (error) {
+//         return response.status(403).send({
+//           success: false,
+//           messgae: 'Invalid authorization token',
+//         });
+//       } else {
+//         request.decoded = decoded;
+//         next();
+//       }
+//     });
+//   } else {
+//     return response.status(403).send({
+//       success: false,
+//       messgae: 'You must be authorized to hit this end point',
+//     });
+//   }
+// };
+//
+// module.exports = checkAuth;
