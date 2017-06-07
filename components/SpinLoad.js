db10560fd2571a44d250cc8defbec2f97df05808
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-06-06 18:28:21 -0600

    Add component SpinLoad

---
+++ components/SpinLoad.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Spinner } from 'native-base';
+
+export default class SpinLoad extends Component {
+  render() {
+    return (
+      <Spinner color='blue'/>
+
+    );
+  }
+}
